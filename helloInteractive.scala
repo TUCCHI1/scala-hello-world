20d2a025236cf6594ec5a05cb98a38f474bf4431
TUCCHI1 <tutikikin1@gmail.com>
2024-11-22 00:56:42 +0900

    Updates variable declarations with numeric types
    Replaces string and custom type declarations with primitive numeric types (Byte, Int, Long, Short, Float) to demonstrate different numeric data type usage in Scala.
    
    Removes unused import statement for StdIn.readLine

--- helloInteractive.scala
+++ helloInteractive.scala
@@ -1,6 +1,6 @@
-import scala.io.StdIn.readLine
-
 @main def helloInteractive() =
-    val x: Int = 1
-    val s: String = "a string"
-    val p: Person = Person("Richard")
+    val b: Byte = 1
+    val i: Int = 1
+    val l: Long = 1
+    val s: Short = 1
+    val d: Float = 3.0
